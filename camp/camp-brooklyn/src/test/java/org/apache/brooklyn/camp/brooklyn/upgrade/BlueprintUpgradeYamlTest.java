b5b792af62e3f49bf5dace3bf126a3500c0c0ccd
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2017-03-24 12:17:42 +0000

    Test example from discussion doc.

--- camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/upgrade/BlueprintUpgradeYamlTest.java
+++ camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/upgrade/BlueprintUpgradeYamlTest.java
@@ -20,16 +20,24 @@
 package org.apache.brooklyn.camp.brooklyn.upgrade;
 
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.util.regex.Pattern;
+
+import javax.annotation.Nullable;
 
 import org.apache.brooklyn.api.entity.Entity;
 import org.apache.brooklyn.api.entity.EntitySpec;
 import org.apache.brooklyn.camp.brooklyn.AbstractYamlTest;
+import org.apache.brooklyn.core.config.ConfigKeys;
 import org.apache.brooklyn.core.upgrade.EntityAndSpecMatcher;
 import org.apache.brooklyn.core.upgrade.Modification;
 import org.apache.brooklyn.core.upgrade.ModificationGeneratingCallback;
 import org.apache.brooklyn.util.core.ResourceUtils;
 import org.testng.annotations.Test;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.Iterables;
 
 public class BlueprintUpgradeYamlTest extends AbstractYamlTest {
@@ -80,6 +88,54 @@ public class BlueprintUpgradeYamlTest extends AbstractYamlTest {
         for (Modification mod : callback.getPlan().getModifications()) {
             System.out.println(mod);
         }
+    }
+
+
+    @Test
+    public void testExampleFromDiscussionDoc() throws Exception {
+        loadIntoCatalogue("classpath://upgrades/example-from-discussion-doc.v1.yaml");
+        Entity app = loadAndStart("classpath://upgrades/example-from-discussion-doc-app.v1.yaml");
+        final Entity testEntity = Iterables.getOnlyElement(app.getChildren());
+        testEntity.config().set(ConfigKeys.newIntegerConfigKey("keyC"), 9);
+
+        loadIntoCatalogue("classpath://upgrades/example-from-discussion-doc.v2.yaml");
+
+        EntitySpec<?> spec = loadSpec("classpath://upgrades/example-from-discussion-doc-app.v2.yaml");
+        final EntitySpec<?> childSpec = Iterables.getOnlyElement(spec.getChildren());
+
+        ModificationGeneratingCallback callback = new ModificationGeneratingCallback(true);
+        EntityAndSpecMatcher matcher = new EntityAndSpecMatcher(callback);
+
+        matcher.match(testEntity, childSpec);
+        final Iterable<Modification> modifications = callback.getPlan().getModifications();
+        assertFalse(Iterables.isEmpty(modifications), "no modifications created");
+
+        for (Modification mod : modifications) {
+            System.out.println(mod);
+        }
+        final Modification cat = Iterables.find(modifications, contains("Change catalog item id"));
+        assertTrue(cat.toString().contains(childSpec.getCatalogItemId()));
+
+        final Modification reset = Iterables.find(modifications, contains("Reset configuration"));
+
+        assertTrue(reset.toString().contains("keyA=2"), "Unexpected upgraded config value");
+
+        // maybe TODO? keyB=9 (spec overrides are kept)
+        assertTrue(reset.toString().contains("keyB=2"), "Unexpected upgraded config value");
+
+        // maybe TODO? keyC=9 (out-of-band change is kept)
+        assertTrue(reset.toString().contains("keyC=2"), "Unexpected upgraded config value");
+
+        assertTrue(reset.toString().contains("keyD=2"), "Unexpected upgraded config value");
+    }
+
+    private Predicate<Modification> contains(final String text) {
+        return new Predicate<Modification>() {
+            @Override
+            public boolean apply(@Nullable Modification input) {
+                return input.toString().contains(text);
+            }
+        };
     }
 
     private Entity loadAndStart(String resource) throws Exception {
